d7507a33d127e98565eb99df610b3557374e3485
COVID-19 HLA AND IMMUNOGENETICS CONSORTIUM <76015809+COVID-HLA@users.noreply.github.com>
2021-09-27 17:09:58 -0400

    Upload R script
    Minor changes for compatibility

--- ASHI.R
+++ ASHI.R
@@ -2,9 +2,12 @@
 ASHItoy <- read.delim("~/Documents/ASHI2021/ASHItoy.txt", stringsAsFactors=FALSE)
 View(ASHItoy)
 
+###Make an output directory
+dir.create("~/Documents/ASHI2021/ASHImainres")
+
 ###run main analysis:Hardy Weinberg, all alleles at all loci, extended haploype all loci, amino acid level
 library(BIGDAWG)
-BIGDAWG("ASHItoy.txt", Run.Tests = c("HWE","L","H","A"), Results.Dir = "~/Documents/ASHI2021/ASHImainres")
+BIGDAWG(ASHItoy, Run.Tests = c("HWE","L","H","A"), Results.Dir = "~/Documents/ASHI2021/ASHImainres")
 
 ###Read in and view main per allele result
 Locus_ORmain <- read.delim("~/Documents/ASHI2021/ASHImainres/Set1/Locus_OR.txt", stringsAsFactors=FALSE)
@@ -18,8 +21,11 @@ correct<-corrA+corrB+corrDR
 Locus_ORmain$p_corr<-Locus_ORmain$p.value*correct
 Locus_ORmain$sig_corr<-ifelse(Locus_ORmain$p_corr<0.05,"*","NS")
 
+### make a field-resolution directory
+dir.create("~/Documents/ASHI2021/ASHI1fieldres")
+
 ###one-field results
-BIGDAWG("ASHItoy.txt", Run.Tests = "L", Trim = TRUE, Res = 1,Results.Dir = "~/Documents/ASHI2021/ASHI1fieldres")
+BIGDAWG(ASHItoy, Run.Tests = "L", Trim = TRUE, Res = 1,Results.Dir = "~/Documents/ASHI2021/ASHI1fieldres")
 
 ###Read in and view 1-field result
 Locus_OR1field <- read.delim("~/Documents/ASHI2021/ASHI1fieldres/Set1/Locus_OR.txt", stringsAsFactors=FALSE)
@@ -33,8 +39,11 @@ correct<-corrA+corrB+corrDR
 Locus_OR1field$p_corr<-Locus_OR1field$p.value*correct
 Locus_OR1field$sig_corr<-ifelse(Locus_OR1field$p_corr<0.05,"*","NS")
 
+### make a haplotype-resolution directory
+dir.create("~/Documents/ASHI2021/ASHIhapres")
+
 ###DR-DQ haplotypes
-BIGDAWG("ASHItoy.txt", Run.Tests = "H",Loci.Set = list(c("DRB1","DQB1")), Results.Dir = "~/Documents/ASHI2021/ASHIhapres")
+BIGDAWG(ASHItoy, Run.Tests = "H",Loci.Set = list(c("DRB1","DQB1")), Results.Dir = "~/Documents/ASHI2021/ASHIhapres")
 
 ###Read in and view DR~DQ haplotype result
 Haplotype_OR <- read.delim("~/Documents/ASHI2021/ASHIhapres/Set1/haplotype_OR.txt", stringsAsFactors=FALSE)
